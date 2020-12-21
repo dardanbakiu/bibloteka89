9be4d49bd8e5744656f38acd4b28870d3cb589d1
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-12-21 16:56:25 +0100

    admin route

--- router/adminRoute.js
+++ router/adminRoute.js
@@ -5,4 +5,9 @@ router.get('/admin', (req, res) => {
     res.render('admin.ejs')
 })
 
+router.post('/adminLoginForm', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+})
+
 module.exports = router
