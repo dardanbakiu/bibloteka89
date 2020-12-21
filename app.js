4f4b43307ff47af464f8d504dcecb4f8e2b709de
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-12-21 17:24:21 +0100

    admin route

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 app.set('view engine', 'ejs')
+const session = require('express-session')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/static', express.static('static'))
 const home = require('./router/homeRouter')
 const admin = require('./router/adminRoute')
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 30 * 60 * 1000 } //30 minuta bablock
+}))
 
 app.use(home)
 app.use(admin)
--- router/adminRoute.js
+++ router/adminRoute.js
@@ -2,12 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/admin', (req, res) => {
-    res.render('admin.ejs')
+    res.render('admin.ejs', { loginError: '' })
 })
 
 router.post('/adminLoginForm', (req, res) => {
+    console.log(req.body)
     const username = req.body.username
     const password = req.body.password
+
+    if (username == process.env.ADMIN_USER && password == process.env.ADMIN_PW) {
+        req.session.admin = username
+        res.redirect('/register')
+    }
+
+    else {
+        res.render('admin', { loginError: 'wrong usr/pw' })
+    }
 })
 
 module.exports = router
