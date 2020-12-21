63a5cd8d14b608e12e9a8b00e1964cbcf4ba6f37
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-12-21 16:49:30 +0100

    admin

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 require('dotenv').config()
 app.set('view engine', 'ejs')
 
-
 app.use('/static', express.static('static'))
 const home = require('./router/homeRouter')
+const admin = require('./router/adminRoute')
+
 app.use(home)
+app.use(admin)
 
 app.listen(3000)
